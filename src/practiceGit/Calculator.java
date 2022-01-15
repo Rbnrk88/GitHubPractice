9c6df9d77599e00fdbf7bb23788d28b973cbd864
Rabia Kayan <93150642+Rbnrk88@users.noreply.github.com>
2022-01-15 09:28:39 -0600

    created divide method

--- src/practiceGit/Calculator.java
+++ src/practiceGit/Calculator.java
@@ -14,7 +14,10 @@ public class Calculator {
         int result= num1-num2;
         return result;
     }
-
+    public static int divide(int num1, int num2){
+        int result= num1/num2;
+        return result;
+    }
 
 
 
